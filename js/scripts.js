57edd323b40e2ae7b4f4ff980d306a3da900a9a9
konstantos <sunnyday-23@mail.ru>
2022-10-16 13:34:08 +0300

    feat: add footer

---
+++ js/scripts.js
@@ -0,0 +1,2 @@
+$("#footer-hola-sites").on("click", function() {$("#footer-hola-sites, #footer-hola-sites img, #footer-hola-sites nav").toggleClass("active")});
+$("#footer-int-sites").on("click", function() {$("#footer-int-sites, #footer-int-sites img, #footer-int-sites nav").toggleClass("active")});
